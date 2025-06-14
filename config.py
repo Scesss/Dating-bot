60781ef7cbd2ade5a5ffb16f563e423911b68765
Scesss <kiriell55@gmail.com>
2025-06-14 20:09:01 +0300

    bot X yandex

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ load_dotenv()
 
 class Config:
     BOT_TOKEN = os.getenv("BOT_TOKEN")
+    YANDEX_GEOCODER_API_KEY = os.getenv("YANDEX_GEOCODER_API_KEY")
     # Add other configs later (database URL, etc.)
-
